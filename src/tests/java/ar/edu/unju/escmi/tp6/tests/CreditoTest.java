12d4c875ad2a31452a9cab5af0c18fb914320935
tomas <tomas@TomasGudi>
2024-10-29 20:00:40 -0300

    Cambios para el recuperatorio del tp6

--- src/tests/java/ar/edu/unju/escmi/tp6/tests/CreditoTest.java
+++ src/tests/java/ar/edu/unju/escmi/tp6/tests/CreditoTest.java
@@ -19,14 +19,17 @@ class CreditoTest {
     @BeforeEach
     void setUp() {
     	Cliente cliente = new Cliente(45111222, "Mario Barca", "Alvear 120", "65454686");
-    	Producto producto = new Producto(1111, "Aire Acondicionado Split On/Off 2750W FC Hisense", 220000, "Argentina");
-    	List<Detalle> detalles = new ArrayList<Detalle>();
-    	
-    	Factura factura = new Factura(LocalDate.now(), 1234, cliente, detalles );
-    	TarjetaCredito tarjeta = new TarjetaCredito(1234, LocalDate.now(), cliente, 123);
-    	credito = new Credito();
-        credito.setFactura(factura);
-        credito.setTarjetaCredito(tarjeta);
+        Producto producto = new Producto(1111, "Aire Acondicionado Split On/Off 2750W FC Hisense", 220000, "Argentina");
+        List<Detalle> detalles = new ArrayList<Detalle>();
+        detalles.add(new Detalle(12,10000,producto));
+
+        this.factura = new Factura(LocalDate.now(), 1234, cliente, detalles);
+        this.tarjeta = new TarjetaCredito(1234, LocalDate.now(), cliente, (long)1200);
+
+        List<Cuota> cuotas = new ArrayList<Cuota>();
+        cuotas.add(new Cuota(9000, 5678, LocalDate.now(), LocalDate.now().plusMonths(1)));
+
+        this.credito = new Credito(this.tarjeta, this.factura, cuotas);
     }
     
     
--- src/tests/java/ar/edu/unju/escmi/tp6/tests/CuotaTest.java
+++ src/tests/java/ar/edu/unju/escmi/tp6/tests/CuotaTest.java
@@ -2,6 +2,8 @@ package ar.edu.unju.escmi.tp6.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,11 +13,24 @@ import ar.edu.unju.escmi.tp6.dominio.*;
 class CuotaTest {
 	
 	 	private Credito credito;
+		private Factura factura;
+		private TarjetaCredito tarjeta;
 		
 	 	@BeforeEach
 	    public void setUp() {
-	        credito = new Credito();
-	        credito.generarCuotas();
+	 		Cliente cliente = new Cliente(45111222, "Mario Barca", "Alvear 120", "65454686");
+	        Producto producto = new Producto(1111, "Aire Acondicionado Split On/Off 2750W FC Hisense", 220000, "Argentina");
+	        List<Detalle> detalles = new ArrayList<Detalle>();
+	        detalles.add(new Detalle(12,10000,producto));
+
+	        this.factura = new Factura(LocalDate.now(), 1234, cliente, detalles);
+	        this.tarjeta = new TarjetaCredito(1234, LocalDate.now(), cliente, (long)1200);
+
+	        List<Cuota> cuotas = new ArrayList<Cuota>();
+	        cuotas.add(new Cuota(9000, 5678, LocalDate.now(), LocalDate.now().plusMonths(1)));
+
+	        this.credito = new Credito(this.tarjeta, this.factura, cuotas);
+	        this.credito.generarCuotas();
 	    }
 
 	    @Test
